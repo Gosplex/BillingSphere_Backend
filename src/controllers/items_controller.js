bc2f8cf93d997de606f14fae718dc5c76d963ba3
Gosplex <johngospel003@gmail.com>
2024-07-27 16:31:03 +0530

    added compression package

--- src/controllers/items_controller.js
+++ src/controllers/items_controller.js
@@ -34,7 +34,7 @@ const fetchAllItems = async (req, res) => {
 const getItems = async (req, res) => {
   try {
     let page = parseInt(req.query.page) || 1;
-    let limit = parseInt(req.query.limit) || 1500;
+    let limit = parseInt(req.query.limit) || 100;
 
     // Get companyCode from params
     const { companyCode } = req.params;
